8536c281b6b82ab1a1fc44efaed115daa13c3c64
castcoco <castcoco@hotmail.com>
2017-07-03 21:34:31 +0100

    Add files via upload

--- demojs3.js
+++ demojs3.js
@@ -27,19 +27,45 @@
 
 var dragImage = document.getElementById("barEle");
 var dropLoc = document.getElementById("container-template");
+var draggable = document.getElementById("bar");
+
+
+// function dragStartHandle(evt){
+// 	console.log("it starts dragging");
+// 	evt.dataTransfer.setData('key', evt.target.id);
+// 	//evt.dataTransfer.setDragImage(img, evt.target.id);
+// 	evt.dropEffect ="move"
+// 	//var img = new Image();
+// 	//img.src = "images/testherbslcon.png";
+	
+// }
 
 dragImage.ondragstart = function(evt){   //drag is started
-	evt.dataTransfer.setData('key',evt.target.id);
+	evt.dataTransfer.setData('text',evt.target.id);
 	console.log("it's dragging");
 }
+
+// function dragOverHandle(evt){
+// 	evt.preventDefault();
+// 	console.log("it's dragging over?!");
+// }
 
 dropLoc.ondragover = function(evt){    //drag over on canvas
 	evt.preventDefault();
 	console.log("it's dragging over?!");
 }
 
+// function dropHandler(evt){
+// 	console.log("start dropping")
+// 	evt.preventDefault();
+// 	var element = evt.dataTransfer.getData("key");
+// 	evt.target.appendChild(document.getElementById(element));
+// 	evt.dataTransfer.ClearData();
+// 	console.log("done");
+// }
+
 dropLoc.ondrop = function(evt){    //drop the item
-	var dropImage = evt.dataTransfer.getData('key'); 
+	var dropImage = evt.dataTransfer.getData('text'); 
 	evt.preventDefault();
 	console.log("it is dropped");
 	console.log(dropImage);
@@ -51,3 +77,57 @@ dropLoc.ondrop = function(evt){    //drop the item
 	console.log("done")
 }
 
+// $(function(){
+// 	$("#barEle").draggable();	
+// });
+
+//dragging object
+
+window.onLoad = addListeners();
+var offX;
+var offY;
+
+
+function addListeners(){
+	document.getElementById("barEle").addEventListener("mousedown", mouseDown, false);
+	window.addEventListener("mouseup", mouseUp, false);
+
+}
+
+
+function mouseUp(){
+	window.removeEventListener("mousemove", divMove, true);
+}
+
+function mouseDown(){
+	window.addEventListener("mousemove", divMove, true);
+}
+
+function divMove(){
+	evt = window.event || evt; 
+	var div = document.getElementById("barEle");
+	div.style.position = "absolute";
+	div.style.top = evt.clientY + "px";
+	div.style.left = evt.clientX + "px";
+}
+
+
+// changing the size of container
+function formSaved(){
+	var selWidth = $('#widthmenu').find(":selected").attr("value");
+	var selLength = $('#lengthmenu').find(":selected").attr("value");
+}
+
+
+
+//change the content of toolbar
+$(document).on("change", "#categoriesSelect", function(){
+	var target = $(this).data("target");
+	var showing = $("option:selected", this).data("show");
+	$(target).children().addClass("hide");
+	$(showing).remove("hide");
+});
+
+$(document).ready(function(){
+	$("#categoriesSelect").trigger("change")
+})
