2dedb36b2721b01a7c34eed0a0627aa7da1e77a1
castcoco <castcoco@hotmail.com>
2017-07-05 16:34:46 +0100

    Update dnd.js

--- dnd.js
+++ dnd.js
@@ -1,37 +1,15 @@
+$('#containerDrop').droppable({
+  accept:'.dragObject',
+  drop: function(ev, ui) { 
+    $(ui.draggable).clone(true).appendTo(this);
+    
+  } 
+});
 
-var x = null;
-
-	// object dragable
-	$(".dragObject").draggable({
-		helper:'clone',
-		cursor:'move',
-		tolerant:'fit',
-		revert:true
-	});
+//resize the dragged item in container
+$(function(){
+	$("#draggable").resizable();
+})
 
 
-	$('#containerDrop').droppable({
-		accept:'.dragObject',
-		drop:function (e, ui){
-			if($(ui.draggable)[0].id !=""){
-				x = ui.helper.clone();
-				ui.helper.remove();
-				x.draggable({
-					helper:'original',
-					cursor:'move',
-					tolerant:'fit',
-					drop:function(event,ui){
-						$(ui.draggable).remove();
-						$(this).find("p").html("Yes, dropped")
-
-					}
-				});
-				x.resizable({
-                    maxHeight: $('#containerDrop').height(),
-                    maxWidth: $('#containerDrop').width()
-                });
-			}
-		}
-	});
-	
 	
